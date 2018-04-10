255eb98d516fc55477c885b51bb49b760f969039
Philam18 <philiplam818@gmail.com>
2018-04-10 17:53:12 -0400

    added .gitignore

--- Client.java
+++ Client.java
@@ -4,29 +4,28 @@ import java.net.*;
 
 
 public class Client {
-  
+
   public static void main(String[] args) throws IOException {
     String sentence;
     String modifiedSentence;
-    
+
     BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
     InetAddress ip = InetAddress.getByName("127.0.0.1");
     Socket clientSocket = new Socket(ip, 40280);
-    
+
     DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-    
+
     BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    
+
     sentence = inFromUser.readLine();
-    
+
     outToServer.writeBytes(sentence + '\n');
-    
+
     modifiedSentence = inFromServer.readLine();
-    
-System.out.println("FROM SERVER: " + modifiedSentence);
-clientSocket.close();
+
+    System.out.println("FROM SERVER: " + modifiedSentence);
+    clientSocket.close();
     
   }
-  
-}
 
+}
