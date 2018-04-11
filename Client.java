7493c4f27f5a9f49ba8ac590db021a69cdbda1c9
jedtopacio <jtopacio@ryerson.ca>
2018-04-11 16:30:37 -0400

    uses the GUI frame - functionality still not done tho

--- Client.java
+++ Client.java
@@ -1,65 +1,80 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.regex.*;
 import java.lang.*;
+
 
 public class Client {
 
   public static void main(String[] args) throws IOException {
     String inputFilename;
+    String html = "";
 
-    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
     InetAddress ip = InetAddress.getByName("127.0.0.1");
     Socket clientSocket = new Socket(ip, 40280);
 
-    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-
+    PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
     BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-    PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
+    System.out.println("Connecting to web server...");
+    System.out.println("");
+    
+    pw.println("GET / HTTP/1.1");
+    pw.println("Host: 127.0.0.1:40280");
+    pw.println("User-Agent: Phil, Ron and Jed's Client App");
+    pw.println("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+    pw.println("Accept-Language: en-US,en;q=0.5");
+    pw.println("Accept-Encoding: gzip, deflate");
+    pw.println("Connection: keep-alive");
+    pw.println("Upgrade-Insecure-Requests: 1");
+    pw.println("");
 
-    // System.out.print("Please enter the file name that you want to request from the server");
-    // System.out.println("(or press \'Enter\'/blank for index.html): ");
-    // inputFilename = inFromUser.readLine();
-    // if(inputFilename.isEmpty()){
-    //   System.out.println("TRUE");
-    //   inputFilename = " ";
-    // }
+    pw.flush();
 
-    inputFilename = "index.html";
-    if (!inputFilename.isEmpty()){
-      System.out.println("GET /" + inputFilename + " HTTP/1.1");
-      pw.println("GET /" + inputFilename + " HTTP/1.1");
-      pw.println("Host: 127.0.0.1:40280");
-      pw.println("User-Agent: Phil, Ron and Jed's Client App");
-      pw.println("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-      pw.println("Accept-Language: en-US,en;q=0.5");
-      pw.println("Accept-Encoding: gzip, deflate");
-      pw.println("Connection: keep-alive");
-      pw.println("Upgrade-Insecure-Requests: 1");
-      pw.println("");
-
-
-      pw.flush();
-
-      String t;
-      while((t = inFromServer.readLine()) != null){
-        System.out.println(t);
-      }
-
-      pw.close();
-      inFromServer.close();
-
-      clientSocket.close();
-
-
+    String message;
+    while((message = inFromServer.readLine()) != null){
+       html += message + '\n';
     }
+    
+    System.out.println("------------- RECEIVED HTML -------------");
+    System.out.println(html);
+    System.out.println("----------------------------------------" + '\n');
+    
+    ArrayList urlLinks = parseHTML(html);
+
+    /**if (!urlLinks.isEmpty()){
+      System.out.println("We have found " + urlLinks.size() + " links in the page.");
+      System.out.println("Please enter the number of the link you want to visit: ");
+      for (int i=0; i < urlLinks.size(); i++){
+        System.out.println("   " + i + " = " + urlLinks.get(i));
+      }
+    }*/
+    
+    pw.close();
+    inFromServer.close();
+
+    clientSocket.close();
+    
+    AppFrame f = new AppFrame(urlLinks);
+    f.setVisible(true);
+    
   }
+  
+  public static void printH(String test){
+    System.out.println(test);
+  }
+  
 
   private static ArrayList<String> parseHTML(String html){
+
     ArrayList<String> links = new ArrayList<String>();
-    String regex = "(href=\"){1}[A-Za-z0-9\.\/]+(\"){1}";
     
+    Pattern p = Pattern.compile("<a[^>]+href=[\"']?([^>\"']*)[\"']?[^>]*>(.+?)</a>");
+    Matcher m = p.matcher(html);
+    while (m.find()) {
+      links.add(m.group(1));
+    }
     return links;
   }
 }
